4ec3a8ba3f73a9ada5bf665e4fed45c3ee986602
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-05-31 22:49:51 +0200

    add status and id

--- app/routes/remittance/index.js
+++ app/routes/remittance/index.js
@@ -4,28 +4,37 @@ export default Route.extend({
     model() {
         return [
             {
+                id: 1,
                 sender: "John",
                 receiver: "Jill",
                 amount: "5 USD",
-                date: new Date().toISOString()
+                date: new Date().toISOString(),
+                status: "Approve"
             },
             {
+                id: 2,
                 sender: "Phil",
                 receiver: "Pam",
                 amount: "7 USD",
-                date: new Date().toISOString()
+                date: new Date().toISOString(),
+                status: "Approve"
+
             },
             {
+                id: 3,
                 sender: "Alex",
                 receiver: "Anna",
                 amount: "8 USD",
-                date: new Date().toISOString()
+                date: new Date().toISOString(),
+                status: "Done"
             },
             {
+                id: 4,
                 sender: "Jemal",
                 receiver: "Zubeir",
                 amount: "9 USD",
-                date: new Date().toISOString()
+                date: new Date().toISOString(),
+                status: "Done"
             }
         ]        
     }
